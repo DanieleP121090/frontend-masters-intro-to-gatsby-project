4ca3ff66fadca37e7229de416027d68e95ec7dfe
Daniele Petrarolo <daniele.petrarolo@zucchetti.it>
2023-05-17 17:57:23 +0200

    Modifiche varie

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,39 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import Layout from "../components/layout.js";
 
 export default function IndexPage() {
+
+  const data = useStaticQuery(graphql`
+      query GetBlogPosts {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+          nodes {
+            id
+            slug
+            frontmatter {
+              date
+              description
+              title
+            }
+          }
+        }
+      }
+  `);
+
+
+  const posts = data.allMdx.nodes;
+  console.log(posts);
+
+
   return (
     <Layout>
         <h1>Hello Frontend Masters!!!</h1>
         <Link to="/about">link to about</Link>
+
+        <h2>Check my blog</h2>
+        <ul>
+          { posts.map((post) => ( <li key={post.id}><Link to={post.slug}>{post.frontmatter.title}</Link></li> )) }
+        </ul>
     </Layout>
   );
 }
